e93bcd5b0a0be0037a83a009236007d035c80df8
solosolius <soliusaltijddemijne@gmail.com>
2022-02-13 18:01:31 +0800

    ladder problem via recursion

---
+++ concepts/recursion/quick_thinking/steps.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int steps(int s,int N) {
+	//base case 
+	if(N == 0) {
+		return 1;
+	}
+	
+
+	//rec case
+	int count = 0;
+	for(int i = 1;i<=s;i++) {
+		if(N >= i) {
+			count+=steps(s,N-i);
+		}
+	}
+	return count;
+}
+
+
+int main() {
+	int ways = steps(3,4);
+	cout<<ways<<endl;
+}
