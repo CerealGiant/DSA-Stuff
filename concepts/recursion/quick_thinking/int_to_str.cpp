7bd518ef3f72dc19640a624d02e01f5b070a7e6d
solosolius <soliusaltijddemijne@gmail.com>
2022-02-13 16:36:08 +0800

    string to integer conversion using recursion

---
+++ concepts/recursion/quick_thinking/int_to_str.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+#include <math.h>
+using namespace std;
+
+
+int stow(char* s,int len) {
+	//base case
+	if(len == 0) {
+		return 0;
+	}
+
+	return (s[0]-'0')*pow(10,len-1) + stow(s+1,len-1);
+}
+
+
+int main() {
+	char a[] = {'1','2','3','4','\0'};
+	int len = strlen(a);
+	int number = stow(a,len);
+	cout<<number<<endl;
+
+}
