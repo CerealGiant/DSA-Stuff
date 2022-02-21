5c91653b1c9ac7ab0eb5bd859d686c4fbd4489c3
solosolius <soliusaltijddemijne@gmail.com>
2022-02-21 11:31:31 +0800

    CPP Inbuilt Sorting

---
+++ concepts/arrays/sort_methods/inbuilt_sort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compare(int a,int b) {
+	return a > b;
+	//If you say a < b then it will be increasing order.
+}
+
+int main() {
+	int a[] = {1,5,4,2,8,10};
+	int n = sizeof(a)/sizeof(int);
+	sort(a,a+n); //Inbuit STL Sort
+	for(auto x: a) cout<<x<<" ";
+	cout<<endl;
+	//If you want it in decreasing order you need to write a compartor function and add it in sort
+	sort(a,a+n,compare); //Inbuit STL Sort
+	for(auto x: a) cout<<x<<" ";
+}
